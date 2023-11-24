d9f4432e45c620ae0a3f3836e677fb167a323d4b
alu0101056944 <alu0101056944@ull.edu.es>
2023-11-24 12:02:22 +0000

    Added a delete main.css task to avoid sourcemaps concatenation.

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -4,38 +4,44 @@ const { src, dest, task, series, watch } = require('gulp');
 const concatCss = require('gulp-concat-css');
 const minifyCss = require('gulp-minify-css');
 const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
 
 const browserSync = require('browser-sync').create();
 
 const gulpSass = require('gulp-dart-sass');
 
-task('compilesass', () => {
-  return src('app/styles/*.scss')
-      .pipe(gulpSass
-          .sync({ outputStyle: 'compressed' }) // faster than async
-          .on('error', gulpSass.logError))
-      .pipe(dest('app/styles'));
+task('deleteMain', () => {
+  return del(['app/styles/main.css']);
 });
+
+task('compilesass', () => {
+      return src('app/styles/*.scss')
+          .pipe(gulpSass
+              .sync({ outputStyle: 'compressed' }) // faster than async
+              .on('error', gulpSass.logError))
+          .pipe(dest('app/styles'));
+    });
 
 task('processCss', () => {
-  return src('./app/styles/*.css')
-      .pipe(concatCss('main.css'))
-      .pipe(sourcemaps.init())
-      .pipe(minifyCss())
-      .pipe(sourcemaps.write())
-      .pipe(dest('app/styles'));
-});
+      return src('./app/styles/*.css')
+          .pipe(concatCss('main.css'))
+          .pipe(sourcemaps.init())
+          .pipe(minifyCss())
+          .pipe(sourcemaps.write())
+          .pipe(dest('app/styles'));
+    });
 
 task('activate-browser-sync', () => {
-  browserSync.init({
-    server: {
-      baseDir: './app/',
-    }
-  });
-});
+      browserSync.init({
+        server: {
+          baseDir: './app/',
+        }
+      });
+    });
 
-watch('app/styles/*.*', series('compilesass', 'processCss', browserSync.reload));
+task('default', series('deleteMain', 'compilesass', 'processCss',
+    'activate-browser-sync'));
 
-task('default', series('compilesass', 'processCss', 'activate-browser-sync'));
-
-
+// try app/styles/* instead
+watch('app/styles/*.*', series('deleteMain', 'compilesass', 'processCss',
+    browserSync.reload));
