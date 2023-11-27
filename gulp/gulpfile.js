19ad7546eb6caeb8ead3926434a33d5cc0e47688
alu0101056944 <alu0101056944@ull.edu.es>
2023-11-27 23:27:41 +0000

    Neither gulp minify nor gulp image optimization plugins work.

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,5 +1,5 @@
 
-const { src, dest, task, series, watch } = require('gulp');
+const { src, dest, series, watch } = require('gulp');
 
 const concatCss = require('gulp-concat-css');
 const minifyCss = require('gulp-minify-css');
@@ -7,48 +7,62 @@ const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 const gulpSass = require('gulp-dart-sass');
 const del = require('del');
+// const imageop = require('gulp-image-optimization');
+// const gulpMinify = require('gulp-minify');
 
-function cleanup(done) {
-  del(['app/styles/main.css']);
-  done();
+async function cleanup() {
+  return del(['app/styles/main.css']);
 }
 
-function compileSass(done) {
-  src('app/styles/*.scss')
+async function compileSass() {
+  return src('app/styles/*.scss')
       .pipe(gulpSass
           .sync({ outputStyle: 'compressed' }) // faster than async
           .on('error', gulpSass.logError))
       .pipe(dest('app/styles'));
-  
-  done();
 };
 
-function processCss(done) {
-  src(['./app/styles/*.css', '!./app/styles/main.css'])
+async function processCss() {
+  return src(['./app/styles/*.css', '!./app/styles/main.css'])
       .pipe(concatCss('main.css'))
       .pipe(sourcemaps.init())
       .pipe(minifyCss())
       .pipe(sourcemaps.write())
       .pipe(dest('app/styles'))
       .pipe(browserSync.stream());
-
-  done();
 };
 
 async function reload() {
   return browserSync.reload();
 }
 
-exports.default = function(done) {
+// async function optimizeImages() {
+//   return src(['./app/*.png'])
+//       .pipe(imageop({
+//             optimizationLevel: 5,
+//             progressive: true,
+//             interlaced: true
+//           }))
+//       .pipe(dest('app/images'));
+// }
+
+// async function minifyJs() {
+//   return src(['./app/scripts/*.mjs'])
+//       .pipe(gulpMinify())
+//       .pipe(dest('./app/scripts/minified'));
+// }
+
+exports.default = async function() {
   browserSync.init({
         server: {
           baseDir: './app/',
         }
       });
 
+  // await optimizeImages();
+  // await minifyJs();
+
   watch('app/styles/*.scss',  { ignoreInitial: false }, compileSass);
   watch(['app/styles/*.css', '!app/styles/main.css'],
       { ignoreInitial: false }, series(cleanup, processCss, reload));
-
-  done();
 };
